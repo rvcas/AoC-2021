9b1f1e984a4d896ec299bb7665d0697059b88546
rvcas <x@rvcas.dev>
2021-12-02 23:53:08 -0500

    feat: day 2 part 1 & 2

--- src/day02.zig
+++ src/day02.zig
@@ -2,4 +2,59 @@ const std = @import("std");
 
 const data = @embedFile("../data/day02.txt");
 
-pub fn main() anyerror!void {}
+pub fn main() anyerror!void {
+    try partOne();
+    try partTwo();
+}
+
+fn partOne() anyerror!void {
+    var horizontal: i32 = 0;
+    var depth: i32 = 0;
+    var it = std.mem.tokenize(u8, data, "\r\n");
+
+    while (it.next()) |line| {
+        var line_it = std.mem.tokenize(u8, line, " ");
+
+        const direction = line_it.next().?;
+        const amount = try std.fmt.parseInt(i32, line_it.next().?, 10);
+
+        if (std.mem.eql(u8, direction, "forward")) {
+            horizontal += amount;
+        } else if (std.mem.eql(u8, direction, "down")) {
+            depth += amount;
+        } else {
+            depth -= amount;
+        }
+    }
+
+    const result = horizontal * depth;
+
+    std.log.info("{}", .{result});
+}
+
+fn partTwo() anyerror!void {
+    var horizontal: i32 = 0;
+    var depth: i32 = 0;
+    var aim: i32 = 0;
+    var it = std.mem.tokenize(u8, data, "\r\n");
+
+    while (it.next()) |line| {
+        var line_it = std.mem.tokenize(u8, line, " ");
+
+        const direction = line_it.next().?;
+        const amount = try std.fmt.parseInt(i32, line_it.next().?, 10);
+
+        if (std.mem.eql(u8, direction, "forward")) {
+            horizontal += amount;
+            depth += aim * amount;
+        } else if (std.mem.eql(u8, direction, "down")) {
+            aim += amount;
+        } else {
+            aim -= amount;
+        }
+    }
+
+    const result = horizontal * depth;
+
+    std.log.info("{}", .{result});
+}
